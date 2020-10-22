4cfe562ff1a5c4963e36fadaff8e5436fd9bcc14
usamahassankhan <usamahassankhan870@gmail.com>
2020-10-22 23:10:16 +0500

    nice

---
+++ reduc_p/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>  i am app component</h1>
+    </div>
+  );
+}
+
+export default App;
